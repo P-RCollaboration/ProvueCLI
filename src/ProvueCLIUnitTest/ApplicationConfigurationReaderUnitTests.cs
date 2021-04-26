928852eb0ab837f8753bc4db64b3181684b91ac8
Romanus <trueromanus@gmail.com>
2021-04-26 20:19:57 +0300

    * Rename tests project + Added tests for FileService

--- src/ProvueCLIUnitTest/ApplicationConfigurationReaderUnitTests.cs
+++ src/ProvueCLIUnitTest/ApplicationConfigurationReaderUnitTests.cs
@@ -10,7 +10,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Xunit;
 
-namespace ProvueCLIUnitTest {
+namespace ProvueCLITests {
 
 	public class ApplicationConfigurationReaderUnitTests {
 
---
+++ src/ProvueCLIUnitTest/FileServiceIntergationTests.cs
@@ -0,0 +1,99 @@
+﻿using ProvueCLI.FileServices.Implementations;
+using System;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ProvueCLITests {
+	public class FileServiceIntergationTests {
+
+		[Fact]
+		[Trait("Category", "Integration")]
+		public void FileExists_Positive() {
+			// arrange
+			var fileService = new FileService ();
+			var tempFileName = Path.GetTempFileName () + Guid.NewGuid ();
+			File.WriteAllText ( tempFileName, "" );
+
+			// act
+			var result = fileService.FileExists ( tempFileName );
+
+			//assert
+			File.Delete ( tempFileName );
+			Assert.True ( result );
+		}
+
+		[Fact]
+		[Trait ( "Category" , "Integration" )]
+		public void FileExists_Negative () {
+			// arrange
+			var fileService = new FileService ();
+			var tempFileName = Path.GetTempFileName () + Guid.NewGuid();
+
+			// act
+			var result = fileService.FileExists ( tempFileName );
+
+			//assert
+			Assert.False ( result );
+		}
+
+		private record TestModel {
+			public string Name { get; set; }
+		}
+
+		[Fact]
+		[Trait ( "Category" , "Integration" )]
+		public async Task ReadJsonFileAsync_Throw_FileNotFound () {
+			// arrange
+			var fileService = new FileService ();
+			var tempFileName = Path.GetTempFileName () + Guid.NewGuid ();
+
+			// assert
+			await Assert.ThrowsAsync<FileNotFoundException> (
+				async () => {
+					// act
+					await fileService.ReadJsonFileAsync<TestModel> ( tempFileName );
+				}
+			);
+		}
+
+		[Fact]
+		[Trait ( "Category" , "Integration" )]
+		public async Task ReadJsonFileAsync_ReadModel () {
+			// arrange
+			var fileService = new FileService ();
+			var tempFileName = Path.GetTempFileName () + Guid.NewGuid ();
+			await File.WriteAllTextAsync ( tempFileName , "{\"name\": \"Testerka\"}" );
+			var testModel = new TestModel { Name = "Testerka" };
+
+			// act
+			var model = await fileService.ReadJsonFileAsync<TestModel> ( tempFileName );
+			
+			// assert
+			File.Delete ( tempFileName );
+			Assert.Equal ( testModel , model );
+		}
+
+		[Fact]
+		[Trait ( "Category" , "Integration" )]
+		public async Task ReadJsonFileAsync_ReadModel_Throw_InnorrectModel () {
+			// arrange
+			var fileService = new FileService ();
+			var tempFileName = Path.GetTempFileName () + Guid.NewGuid ();
+			await File.WriteAllTextAsync ( tempFileName , "[]" );
+
+			// assert
+			await Assert.ThrowsAsync<JsonException> (
+				async () => {
+					// act
+					await fileService.ReadJsonFileAsync<TestModel> ( tempFileName );
+				}
+			);
+
+			File.Delete ( tempFileName );
+		}
+
+	}
+
+}
